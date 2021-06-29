75e1bf0ebe8480673103e9c15fc82701b21d6840
Justin Miller <jmiller9@gmail.com>
2021-06-29 14:13:17 -0400

    working on 2d array problem

---
+++ 2d_array_ds.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+
+
+    return arr
+
+
+
+if __name__ == '__main__':
+
+    arr = [
+            [1, 1, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ]
+
+    result = hourglassSum(arr)
+    print(str(result) + '\n')
